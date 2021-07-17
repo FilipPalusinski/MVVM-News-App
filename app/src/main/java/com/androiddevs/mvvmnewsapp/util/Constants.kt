5b1ce198297aa4fcebfb34a2f8736f787a6493d4
Filip Palusiński <48621677+FilipPalusinski@users.noreply.github.com>
2021-07-17 14:15:01 +0200

    Add Retrofit Setup

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "c07b1d90c38740efbb9454d9d6f42afeS"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
