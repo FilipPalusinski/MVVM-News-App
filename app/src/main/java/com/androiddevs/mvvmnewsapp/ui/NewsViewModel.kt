d9e1bebfccc1abcbfa35c83b069c0dc712a00ba1
Filip Palusiński <48621677+FilipPalusinski@users.noreply.github.com>
2021-07-23 12:36:32 +0200

    Add searching news functionality

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsViewModel.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsViewModel.kt
@@ -10,8 +10,12 @@ import kotlinx.coroutines.launch
 import retrofit2.Response
 
 class NewsViewModel(val newsRepository: NewsRepository): ViewModel(){
+
     val breakingNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
     var breakingNewsPage = 1
+
+    val searchNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    var searchNewsPage = 1
 
     init{
         getBreakingNews("us")
@@ -23,6 +27,12 @@ class NewsViewModel(val newsRepository: NewsRepository): ViewModel(){
         breakingNews.postValue(handleBreakingNewsResponse(response))
     }
 
+    fun searchNews(searchQuery: String) = viewModelScope.launch{
+        searchNews.postValue(Resource.Loading())
+        val response = newsRepository.searchNews(searchQuery, searchNewsPage)
+        searchNews.postValue(handleSearchNewsResponse(response))
+    }
+
     private fun handleBreakingNewsResponse(response: Response<NewsResponse>) : Resource<NewsResponse> {
         if(response.isSuccessful){
             response.body()?.let{ resultResponse ->
@@ -32,4 +42,12 @@ class NewsViewModel(val newsRepository: NewsRepository): ViewModel(){
         return Resource.Error(response.message())
     }
 
+    private fun handleSearchNewsResponse(response: Response<NewsResponse>) : Resource<NewsResponse> {
+        if(response.isSuccessful){
+            response.body()?.let{ resultResponse ->
+                return Resource.Success(resultResponse)
+            }
+        }
+        return Resource.Error(response.message())
+    }
 }
--- app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -4,5 +4,6 @@ class Constants {
     companion object{
         const val API_KEY = "c07b1d90c38740efbb9454d9d6f42afe"
         const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 }
